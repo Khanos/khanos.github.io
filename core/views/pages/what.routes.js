8788fbcd2cb8ecfdf07367b455d836b8c0f23f56
khanos <epilef.rodriguez@gmail.com>
2017-02-23 16:07:01 -0800

    What i do section : Done!

--- core/views/pages/what.routes.js
+++ core/views/pages/what.routes.js
@@ -2,12 +2,13 @@
     'use strict';
     angular
         .module('pop.what')
-        .config(function($routeProvider) {
+        .config(function($routeProvider, $locationProvider) {
             $routeProvider
-                .when("/", {
+                .when("/what", {
                     templateUrl: 'core/views/pages/partials/what.html',
                     controller: 'whatCtrl',
                     controllerAs: 'vm'
                 });
+            $locationProvider.html5Mode(true);
         });
 })();
